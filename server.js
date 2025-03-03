43cd13a3400c090f293286802c7b72f1c26eaa27
Suraj Yadav <codegenerator999@gmail.com>
2025-03-04 01:13:18 +0530

    added index.html

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'Frontend/Sensewell_SBC/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Function to get server IP
 function getIPAddress() {
