f18c1926c1a87f43d8b97ccbf7189ae726dc825b
Suraj Yadav <codegenerator999@gmail.com>
2025-03-04 00:49:53 +0530

    saving ip and sending

--- routes/DeviceRoute.js
+++ routes/DeviceRoute.js
@@ -1,16 +1,17 @@
-module.exports = () => {
-    const express = require('express');
-    const router = express.Router();
+// module.exports = () => {
+//     const express = require('express');
+//     const router = express.Router();
 
-    const sendHello = async(req,res) => {
-        return res.status(201).json({ message: 'Hello'});
-    }
+//     const sendHello = async(req,res) => {
+//         console.log(req.ip);
+//         return res.status(201).json({ message: 'Hello'});
+//     }
 
 
-    router.get('/hello',sendHello);
+//     router.get('/hello',sendHello);
 
-    return router;
-};
+//     return router;
+// };
 
 
 // const express = require('express');
@@ -23,3 +24,32 @@ module.exports = () => {
 
 //   return router;
 // };
+
+const express = require('express');
+const router = express.Router();
+let espIPs = new Map();
+
+router.get('/register', (req, res) => {
+    const clientIP = req.ip || req.headers['x-forwarded-for'];
+    console.log("ESP32 Registered IP:", clientIP);
+    espIPs.set(clientIP, { status: 'online' });
+    res.status(200).json({ message: "ESP Registered", clientIP });
+});
+
+router.get('/control-led', (req, res) => {
+    const clientIP = req.query.ip;
+    const action = req.query.action; // on or off
+
+    if (espIPs.has(clientIP)) {
+        const url = `http://${clientIP}/led?action=${action}`;
+        axios.get(url).then(response => {
+            res.json({ message: `LED ${action}`, response: response.data });
+        }).catch(() => {
+            res.status(500).json({ message: "ESP32 Unreachable" });
+        });
+    } else {
+        res.status(404).json({ message: "Device Not Registered" });
+    }
+});
+
+module.exports = () => router;
